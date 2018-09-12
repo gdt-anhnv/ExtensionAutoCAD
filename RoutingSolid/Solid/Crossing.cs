3ccb81930ffb55160df09d5bc3356fc46c3bb579
anh_nqv <anh_nqv@hicas.com.vn>
2018-09-12 16:10:55 +0700

    solid entity

---
+++ RoutingSolid/Solid/Crossing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutingSolid.Solid
+{
+	class Crossing : SolidEntity
+	{
+		public override void Draw()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ RoutingSolid/Solid/SolidEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutingSolid.Solid
+{
+	abstract class SolidEntity
+	{
+		public abstract void Draw();
+	}
+}
